5ac710448f3f7d18c52964dc109a9b26aea2bb81
Rane Hyvönen <hyvonenrane@gmail.com>
2024-08-29 16:42:57 +0200

    add packages

---
+++ packages/application/src/logger/logger.ts
@@ -0,0 +1,31 @@
+import { config, createLogger, format, transports } from "winston";
+
+const SYSLOG_COLORS: Record<string, string> = {
+  debug: "magenta",
+  info: "white",
+  notice: "green",
+  warning: "yellow",
+  error: "bold red",
+  crit: "inverse yellow",
+  alert: "bold inverse red",
+  emerg: "bold inverse magenta",
+} as const;
+
+const logger = createLogger({
+  levels: config.syslog.levels,
+  format: format.combine(
+    format.timestamp({ format: "DD-MM-YY" }),
+    format.errors({ stack: true }),
+    format.splat(),
+    format.printf(({ timestamp, level, message, ...meta }) => {
+      const metaString = Object.keys(meta).length
+        ? JSON.stringify(meta, null, 2)
+        : "";
+      return `[${timestamp}] [${level}]: ${message} ${metaString}`;
+    }),
+    format.colorize({ all: true, colors: SYSLOG_COLORS })
+  ),
+  transports: [new transports.Console()],
+});
+
+export default logger;
